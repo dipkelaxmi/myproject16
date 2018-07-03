0ab91b8f56f7a8d10e93e75f8c058bdd99266f27
sanyami1998 <sanyami.sonawane@gmail.com>
2018-07-03 11:00:44 +0530

    first commit

---
+++ set16.py
@@ -0,0 +1,21 @@
+#assg:2==set operations
+A=set([1,2,3,4]);
+B=set([1,3,4,5,6,7,8]);
+print("set A:")
+print(A)
+print("length of set A=")
+print(len(A))
+print("set B:")
+print(B)
+print("length of set B=")
+print(len(B))
+print("union of set==")
+print(A.union(B))
+print("intersection of set==")
+print(A.intersection(B))
+print("difference ==")
+print(A-B)
+print("symmetric difference==")
+print(A^B)
+print("difference ==")
+print(B-A)
